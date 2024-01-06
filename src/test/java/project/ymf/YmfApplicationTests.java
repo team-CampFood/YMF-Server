35f1a69cb26c896a0222680899b78efa4104728f
thdgustjd1 <thdgustjd1@naver.com>
2024-01-06 21:32:12 +0900

    test : test파일 추가

---
+++ src/test/java/project/ymf/YmfApplicationTests.java
@@ -0,0 +1,13 @@
+package project.ymf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
