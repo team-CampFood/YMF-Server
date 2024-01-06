7a8d5bc2243cbb21393b1440a0105b96879ac476
thdgustjd1 <thdgustjd1@naver.com>
2024-01-06 22:38:17 +0900

    fix : 불필요한 import 제거

--- src/main/java/project/ymf/src/studyMate/dto/StudyMateAcceptRequest.java
+++ src/main/java/project/ymf/src/studyMate/dto/StudyMateAcceptRequest.java
@@ -2,9 +2,6 @@ package project.ymf.src.studyMate.dto;
 
 import lombok.Getter;
 import project.ymf.common.entity.BaseState;
-import project.ymf.src.singingMate.dto.SingingMateApplies;
-
-import java.util.List;
 
 @Getter
 public class StudyMateAcceptRequest {
