caf6b5daeac68c3888972bc9da359bb26e6bd85f
thdgustjd1 <thdgustjd1@naver.com>
2024-01-06 22:08:47 +0900

    refactor : 신청 수락/거절방식 수정
    ...
    한번에 수락을 하는 방식이 아닌 신청 하나하나를 수락하는 방식으로 변경

--- src/main/java/project/ymf/src/singingMate/dto/SingingMateAcceptRequest.java
+++ src/main/java/project/ymf/src/singingMate/dto/SingingMateAcceptRequest.java
@@ -1,11 +1,13 @@
 package project.ymf.src.singingMate.dto;
 
 import lombok.Getter;
+import project.ymf.common.entity.BaseState;
 import project.ymf.src.singingMate.entity.SingingMateApply;
 
 import java.util.List;
 
 @Getter
 public class SingingMateAcceptRequest {
-    List<SingingMateApplies> data;
+    Long singingMateApplyId;
+    BaseState state;
 }
