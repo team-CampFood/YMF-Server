5984814b0e9b42d2b659fd2687aba98a89063d52
thdgustjd1 <thdgustjd1@naver.com>
2024-01-06 21:51:06 +0900

    feat : 기본 엔티티 정의

---
+++ src/main/java/project/ymf/common/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package project.ymf.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name="updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+
+    @Column(name = "is_deleted", nullable = false)
+    boolean isDeleted;
+    //deleted_at 필요
+
+}
---
+++ src/main/java/project/ymf/common/entity/BaseState.java
@@ -0,0 +1,13 @@
+package project.ymf.common.entity;
+
+public enum BaseState {
+    //mate
+    RECRUITING,
+    FULL,
+    COMPLETED,
+    //subscribe
+    JOINED,
+    APPLYING,
+    REJECTED,
+    NOT_APPLIED;
+}
