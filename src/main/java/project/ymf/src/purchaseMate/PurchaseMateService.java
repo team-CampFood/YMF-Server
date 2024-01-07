2060a0de952e61d3161a0dc1752e9cb6d8c363ce
thdgustjd1 <thdgustjd1@naver.com>
2024-01-07 15:03:58 +0900

    feat : purchaseMate 서비스

---
+++ src/main/java/project/ymf/src/purchaseMate/PurchaseMateService.java
@@ -0,0 +1,42 @@
+package project.ymf.src.purchaseMate;
+
+import org.springframework.stereotype.Service;
+import project.ymf.src.purchaseMate.dto.*;
+
+import java.util.List;
+
+@Service
+public class PurchaseMateService {
+    public void postPurchase(PurchaseMateRequest request) {
+    }
+
+    public void editPurchase(Long purchaseId, PurchaseMateRequest request) {
+    }
+
+    public PurchaseMateInfoDTO getPurchaseInfo(Long purchaseId) {
+    }
+
+    public void applyPurchase(Long purchaseId, PurchaseMateApplyRequest request) {
+    }
+
+    public void deletePurchase(Long purchaseId) {
+    }
+
+    public void completePurchase(Long purchaseId) {
+    }
+
+    public List<PurchaseMateApplyDTO> getPurchaseSubs(Long purchaseId) {
+    }
+
+    public void acceptPurchase(Long purchaseId, PurchaseMateAcceptRequest request) {
+    }
+
+    public List<PurchaseMateDTO> getAllPurchase() {
+    }
+
+    public List<PurchaseMateDTO> getSearchedPurchase(String keyword) {
+    }
+
+    public List<PurchaseMateDTO> getMyPurchase() {
+    }
+}
